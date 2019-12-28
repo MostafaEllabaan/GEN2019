5ba805fe48996f8e3b9c9ea9593b9762a86cc849
MostafaEllabaan <mostafa.mhashim@gmail.com>
2019-12-28 15:38:29 +0100

    Create MultiprocessingUsingPoolAndCommandFile.py

---
+++ Scripts/MultiprocessingUsingPoolAndCommandFile.py
@@ -0,0 +1,15 @@
+
+
+
+from multiprocessing import Pool
+import sys
+import os
+taskfile=sys.argv[1]
+ncpus=int(sys.argv[2])
+pl= Pool(ncpus)
+with open(taskfile) as infile:
+	lines=infile.readlines()
+	pl.map(os.system, lines)
+
+
+
